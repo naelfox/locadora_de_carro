3af7bfce7341638e710f63b4fb2ec757c6a876bb
naelfox <natanaelvila2@gmail.com>
2023-11-25 18:10:52 -0300

    .

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -4,9 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\Marca;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class MarcaController extends Controller
 {
+
+    private $marca;
+
+    public function __construct(Marca $marca)
+    {
+        $this->marca = $marca;
+    }
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +22,7 @@ class MarcaController extends Controller
      */
     public function index()
     {
-        $marcas = Marca::all();
+        $marcas = $this->marca->all();
         return $marcas;
     }
 
@@ -28,19 +36,33 @@ class MarcaController extends Controller
     public function store(Request $request)
     {
         //
-        $marca = Marca::create($request->all());
-        return $marca;
+        $nome =  $request->input('nome');
+        $request->validate($this->marca->rules(), $this->marca->feedback($nome));
+
+        $image = $request->file('imagem');
+
+        $imagemUrn =  $image->store('imagens', 'public');
+
+
+        $marca = $this->marca->create([
+            'nome' => $request->nome,
+            'imagem' => $imagemUrn
+        ]);
+        return  response()->json($marca, 201);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer  $marca
      * @return \Illuminate\Http\Response
      */
-    public function show(Marca $marca)
+    public function show($id)
     {
-
+        $marca = $this->marca->find($id);
+        if (is_null($marca)) {
+            return response()->json(['erro' => 'O recurso não foi encontrado'], 404);
+        }
         return $marca;
     }
 
@@ -49,12 +71,55 @@ class MarcaController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Marca $marca)
+    public function update(Request $request, $id)
     {
-        $marca->update($request->all());
+        $marca = $this->marca->find($id);
+
+        if (is_null($marca)) {
+            return response()->json(['erro' => 'Não foi possível realizar a atualização o recurso não foi encontrado.'], 404);
+        }
+
+
+        $nome =  $request->input('nome');
+
+        if ($request->method() == 'PATCH') {
+
+            $regrasDinamicas = [];
+
+
+            foreach ($marca->rules() as $input => $regra) {
+                if (array_key_exists($input, $request->all())) {
+                    $regrasDinamicas[$input] = $regra;
+                }
+            }
+
+            $request->validate($regrasDinamicas, $marca->feedback($nome));
+        } else {
+
+
+            $request->validate($marca->rules(), $marca->feedback($nome));
+        }
+
+
+
+        if (!is_null($request->file('imagem'))) {
+            Storage::disk('public')->delete($marca->imagem);
+        }
+
+        $image = $request->file('imagem');
+
+        $imagemUrn =  $image->store('imagens', 'public');
+
+
+        $marca->update([
+            'nome' => $request->nome,
+            'imagem' => $imagemUrn
+        ]);
+
+        // $marca->update($request->all());
         return $marca;
     }
 
@@ -64,8 +129,22 @@ class MarcaController extends Controller
      * @param  \App\Models\Marca  $marca
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Marca $marca)
+    public function destroy($id)
     {
+        $marca = $this->marca->find($id);
+        if (is_null($marca)) {
+            return response()->json(
+                [
+                    'erro' => 'Não foi possível excluir o recurso não foi encontrado'
+                ],
+                404
+            );
+        }
+
+
+
+        Storage::disk('public')->delete($marca->imagem);
+
         $marca->delete();
         return ['msg' => 'Marca removida com sucesso'];
     }
--- app/Models/Marca.php
+++ app/Models/Marca.php
@@ -9,4 +9,23 @@ class Marca extends Model
 {
     use HasFactory;
     protected $fillable = ['nome', 'imagem'];
+
+    public function rules()
+    {
+        return  [
+            'nome' => "required|unique:marcas,nome," . $this->id . "|min:3",
+            'imagem' => 'required|file|mimes:png'
+        ];
+    }
+
+    public function feedback($nome)
+    {
+        return [
+            'required' => 'O campo :attribute é obrigatório',
+            'nome.unique' => "O registro '{$nome}' já existe na tabela",
+            'nome.min' => 'O nome deve ter no mínimo 3 caracteres',
+            'imagem.mimes' => 'A extensão aceita tem que ser do tipo .png',
+            'imagem.file' => 'O campo :attribute deve ser um arquivo'
+        ];
+    }
 }
